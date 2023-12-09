331e24acd75d4e2ead9bf3e74811143e7d7aaec5
Syed Zain Anwer <zainanwer24@gmail.com>
2023-12-09 23:46:24 +0100

    minor spelling fix

--- lib/mangoose.ts
+++ lib/mangoose.ts
@@ -1,15 +1,15 @@
-import mangoose from 'mongoose';
+import mongoose from 'mongoose';
 
 let isConnected = false;
 
 export const connectToDB = async() => {
-    mangoose.set('strictQuery', true); // prevent unknown field query
+    mongoose.set('strictQuery', true); // prevent unknown field query
 
     if(!process.env.MONGODB_URL) return console.log("MONGODB_URL not found");
     if(isConnected) return console.log("Already connected to MongoDB");
 
     try {
-        await mangoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(process.env.MONGODB_URL);
 
         isConnected = true;
         console.log("Connected to MongoDB");

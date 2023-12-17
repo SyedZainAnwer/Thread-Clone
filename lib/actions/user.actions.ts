e6846051729aac6d04c382ff4f3ab12dd175a6df
Syed Zain Anwer <zainanwer24@gmail.com>
2023-12-17 15:44:47 +0100

    created activity page and added functionality to it

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -138,3 +138,32 @@ export const fetchUsers = async({
         throw new Error(`Failed to fetch users: ${ error.message }`);
     }
 }
+
+export const getActivity = async(userId: string) => {
+    try {
+        connectToDB();
+
+        // find all threads created by user
+        const userThreads = await Thread.find({ author: userId });
+
+        // Collect all the child thread ids(replies) from the children field
+        const childThreadIds = userThreads.reduce((acc, userThread) => {
+            return acc.concat(userThread.children);
+        }, []); // this [] is a default value not dependency array
+
+        // w want to get the replies except for the user's own comment 
+        const replies = await Thread.find({
+            _id: { $in: childThreadIds },
+            author: { $ne: userId }
+        }).populate({
+            path: 'author',
+            model: User,
+            select: 'name image _id'
+        });
+
+        return replies;
+
+    } catch(error: any) {
+        throw new Error(`Failed to fetch activity: ${error.message}`)
+    }
+}
